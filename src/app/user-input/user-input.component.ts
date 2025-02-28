c4e7ed26967ce1a66478fb931212147d1c594813
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-28 20:42:29 +0100

    Enhance user input component with Bitcoin price card and improved form styling

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,33 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe, DatePipe],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInititialInvestment = signal('0');
-  enteredAnnualInvestment = signal('0');
-  enteredExpectedReturn = signal('5');
+  enteredInititialInvestment = signal('1000');
+  enteredAnnualInvestment = signal('500');
+  enteredExpectedReturn = signal('20');
   enteredDuration = signal('10');
 
-  constructor(private invesmentService: InvestmentService) {}
+  private investmentService = inject(InvestmentService);
+
+  bitcoinPrice = this.investmentService.currentBitcoinPrice;
+  lastUpdated = this.investmentService.lastUpdated;
+  isLoading = this.investmentService.isLoading;
+  error = this.investmentService.error;
+
+  refreshPrice() {
+    this.investmentService.refreshBitcoinPrice();
+  }
 
   onSubmit() {
-    this.invesmentService.onCalculateInvestmentResults({
+    this.investmentService.onCalculateInvestmentResults({
       initialInvestment: +this.enteredInititialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
       duration: +this.enteredDuration(),
     });
 
-    this.enteredInititialInvestment.set('0');
-    this.enteredAnnualInvestment.set('0');
-    this.enteredExpectedReturn.set('5');
+    this.enteredInititialInvestment.set('1000');
+    this.enteredAnnualInvestment.set('500');
+    this.enteredExpectedReturn.set('20');
     this.enteredDuration.set('10');
   }
 }
